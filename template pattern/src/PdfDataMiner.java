5d2ff6e97d0e43ea223c058154311d1aeebc88a2
hassan-wolverine <43892981+hassan-wolverine@users.noreply.github.com>
2021-11-06 12:19:35 +0200

    added my comments

--- template pattern/src/PdfDataMiner.java
+++ template pattern/src/PdfDataMiner.java
@@ -1,5 +1,6 @@
 public class PdfDataMiner extends DataMiner {
 
+    // comment
     public void extractData() {
         System.out.println("extract data from pdf file");
     }
@@ -9,6 +10,7 @@ public class PdfDataMiner extends DataMiner {
         System.out.println("parse data from data extracted from pdf file");
     }
 
+    // comment
     public void analyzeData() {
         System.out.println("analyze data of the pdf file");
     }
